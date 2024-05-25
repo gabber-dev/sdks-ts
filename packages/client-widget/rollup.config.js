4cc296fca163c4e6a9bb331a00b7788a15c2cfb6
Gabber Dev <neil@gofluently.ai>
2024-05-25 15:36:05 -0700

    widget working now

---
+++ packages/client-widget/rollup.config.js
@@ -0,0 +1,28 @@
+const typescript = require("@rollup/plugin-typescript");
+const terser = require('@rollup/plugin-terser');
+const node_resolve = require('@rollup/plugin-node-resolve');
+const common_resolve = require("@rollup/plugin-commonjs");
+const postcss = require("rollup-plugin-postcss")
+const { default: replace } = require("@rollup/plugin-replace");
+
+module.exports = {
+  input: ["src/index.tsx"],
+  output: { dir: "dist", format: "iife", name: "Gabber" },
+  plugins: [
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser(),
+    node_resolve(),
+    common_resolve(),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    postcss({
+      config: { path: "./postcss.config.js" },
+      extensions: [".css"],
+      inject: {
+        insertAt: "top",
+      },
+    }),
+  ],
+};
--- packages/client-widget/tailwind.config.js
+++ packages/client-widget/tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
